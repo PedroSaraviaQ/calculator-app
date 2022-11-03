59433a37ab2ce2a91ec76bd409f3ac99c8b2548e
Pedro <pedro.saravia.q@uni.pe>
2022-11-02 20:57:46 -0500

    four basic arithmetic operations

--- src/App.js
+++ src/App.js
@@ -12,15 +12,18 @@ function App() {
   }
 
   function minus(e) {
-    // Add the code for the minus function
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
   }
 
   function times(e) {
-    // Add the code for the plus function
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
   }
 
   function divide(e) {
-    // Add the code for the divide function
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
   }
 
   function resetInput(e) {
@@ -37,12 +40,12 @@ function App() {
         <h1>Simplest Working Calculator</h1>
       </div>
       <form>
-        <p ref={resultRef}>{/* add the value of the current total */}</p>
+        <p ref={resultRef}>{result}</p>
         <input pattern="[0-9]" ref={inputRef} type="number" placeholder="Type a number" />
         <button onClick={plus}>add</button>
-        {/* Add the subtract button */}
-        {/* Add the multiply button */}
-        {/* Add the divide button */}
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
         {/* Add the resetInput button */}
         {/* Add the resetResult button */}
       </form>
