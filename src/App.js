21ae039e6b1f6d208048b6bf347cd09df935c05c
Pedro <pedro.saravia.q@uni.pe>
2022-11-03 21:29:18 -0500

    added current operation

--- src/App.js
+++ src/App.js
@@ -5,52 +5,52 @@ function App() {
   const inputRef = useRef(null);
   const resultRef = useRef(null);
   const [result, setResult] = useState(0);
+  const [sign, setSign] = useState(null);
 
   function plus(e) {
-    e.preventDefault();
+    setSign("+");
     setResult((result) => result + Number(inputRef.current.value));
   }
 
   function minus(e) {
-    e.preventDefault();
+    setSign("-");
     setResult((result) => result - Number(inputRef.current.value));
   }
 
   function times(e) {
-    e.preventDefault();
+    setSign("×");
     setResult((result) => result * Number(inputRef.current.value));
   }
 
   function divide(e) {
-    e.preventDefault();
+    setSign("÷");
     setResult((result) => result / Number(inputRef.current.value));
   }
 
   function resetInput(e) {
-    e.preventDefault();
     inputRef.current.value = "";
   }
 
   function resetResult(e) {
-    e.preventDefault();
     setResult(0);
   }
 
   return (
     <div className="App">
       <div>
-        <h1>Simplest Working Calculator</h1>
-      </div>
-      <form>
+        <h1>Simplest Working Calculator 🔢📏</h1>
         <p ref={resultRef}>{result}</p>
         <input pattern="[0-9]" ref={inputRef} type="number" placeholder="Type a number" />
+        <p>{sign + inputRef.current.value}</p>
+      </div>
+      <div>
         <button onClick={plus}>add</button>
         <button onClick={minus}>subtract</button>
         <button onClick={times}>multiply</button>
         <button onClick={divide}>divide</button>
         <button onClick={resetInput}>reset input</button>
         <button onClick={resetResult}>reset result</button>
-      </form>
+      </div>
     </div>
   );
 }
